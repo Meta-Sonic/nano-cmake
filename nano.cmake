db5ae25eea0b472bf5a74d2e7f0b8f173090a662
Alexandre Arsenault <aarsenault@landr.com>
2022-07-13 03:06:47 -0400

    Add clang-format

--- nano.cmake
+++ nano.cmake
@@ -8,3 +8,13 @@ function(nano_add_module NAME)
 
   FetchContent_MakeAvailable(${NAME})
 endfunction()
+
+function(nano_clang_format NAME SOURCES)
+  find_program(CLANG_FORMAT clang-format REQUIRED)
+  add_custom_target(${NAME}-formatting DEPENDS ${SOURCES})
+  add_custom_command(TARGET ${NAME}-formatting PRE_BUILD
+      COMMAND "${CLANG_FORMAT}" --Werror  --dry-run ${SOURCES}
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+  add_dependencies(${NAME} ${NAME}-formatting)
+  set_target_properties(${NAME}-formatting PROPERTIES XCODE_GENERATE_SCHEME OFF)
+endfunction()
